52d1f2bbbdd5d393f5eb18730ff377d5095bdab7
tanirachau <90415561+tanirachau@users.noreply.github.com>
2024-10-08 15:12:08 -0700

    Recode Yes/No for q21

--- R/clean_to_process_dr_lausd.R
+++ R/clean_to_process_dr_lausd.R
@@ -16,6 +16,15 @@ clean_to_process_dr_lausd <- function(district,period,raw_file_xwalk) {
       filter(response != 0) %>%
       select(-response)
 
+    # recode Yes/No
+    yes_no_xwalk <- read.csv("./crosswalk/yes_no_xwalk.csv", encoding = 'UTF-8')
+
+    dr_lausd_df <- dr_lausd_df %>%
+      left_join(yes_no_xwalk)
+
+    dr_lausd_df$q21 <- dr_lausd_df$yes_no_clean
+    dr_lausd_df$yes_no_clean <- NULL
+
     dr_lausd_df_process_path <- universal_file_path_helper(
       district = district,
       period = period,
